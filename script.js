4c000ab635f4b5fe288238bd937c971019be379f
Kẏra <kxra@riseup.net>
2018-03-19 15:24:24 -0400

    started a snowman

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
-var scene = new THREE.Scene()
+var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(
-	75,
-	window.innerWidth / window.innerHeight,
-	0.1,
-	1000
-)
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
-var renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(1, 1, 1)
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-var cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+var geometry = new THREE.SphereGeometry(2, 32, 32);
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
 
-camera.position.z = 5
+var geometry2 = new THREE.SphereGeometry(1.75, 32, 32);
+var material2 = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var sphere2 = new THREE.Mesh(geometry2, material2);
+sphere2.position.y = 2;
+scene.add(sphere2);
+
+camera.position.z = 10;
 
 function animate() {
-	requestAnimationFrame(animate)
-	cube.rotation.x += 0.1
-	cube.rotation.y += 0.1
-	renderer.render(scene, camera)
+  requestAnimationFrame(animate);
+  // sphere.rotation.x += 0.2;
+  // sphere.rotation.y += 0.2;
+  renderer.render(scene, camera);
 }
-animate()
+animate();
