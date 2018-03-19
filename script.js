e47de056562b632984573fdf6cf2d946261e39a2
Kẏra <kxra@riseup.net>
2018-03-19 16:01:35 -0400

    added a head and some very pixelated texture

--- script.js
+++ script.js
@@ -1,39 +1,47 @@
-var scene = new THREE.Scene()
+var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(
-	75,
-	window.innerWidth / window.innerHeight,
-	0.1,
-	1000
-)
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
-var renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.SphereGeometry(2, 32, 32)
-var material = new THREE.MeshNormalMaterial({ color: 0x00ff00 })
-var sphere = new THREE.Mesh(geometry, material)
-scene.add(sphere)
+var texture = new THREE.TextureLoader().load("snow.jpg");
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+texture.repeat.set(4, 4);
 
-var geometry2 = new THREE.SphereGeometry(1.75, 32, 32)
-var material2 = new THREE.MeshNormalMaterial({ color: 0x00ff00 })
-var sphere2 = new THREE.Mesh(geometry2, material2)
-sphere2.position.y = 2
-scene.add(sphere2)
+var geometryBase = new THREE.SphereGeometry(6, 32, 32);
+var geometryAbdomen = new THREE.SphereGeometry(5, 32, 32);
+var geometryHead = new THREE.SphereGeometry(4, 32, 32);
+var material = new THREE.MeshBasicMaterial({ color: 0xffffff, map: texture });
+var base = new THREE.Mesh(geometryBase, material);
+var abdomen = new THREE.Mesh(geometryAbdomen, material);
+var head = new THREE.Mesh(geometryHead, material);
+abdomen.position.y = 6.5;
+head.position.y = 12;
+scene.add(base);
+scene.add(abdomen);
+scene.add(head);
 
-camera.position.z = 10
+camera.position.z = 16;
+camera.position.y = 4;
 
 function animate() {
-	requestAnimationFrame(animate)
-	// sphere.rotation.x += 0.2;
-	// sphere.rotation.y += 0.2;
-	renderer.render(scene, camera)
+  requestAnimationFrame(animate);
+  // sphere.rotation.x += 0.2;
+  // sphere.rotation.y += 0.2;
+  renderer.render(scene, camera);
 }
 
-ambientLight = new THREE.AmbientLight(0x333333) // 0.2
-light = new THREE.DirectionalLight(0xffffff, 1.0)
+ambientLight = new THREE.AmbientLight(0x333333); // 0.2
+light = new THREE.DirectionalLight(0xffffff, 1.0);
 
-scene.add(ambientLight)
-scene.add(light)
+scene.add(ambientLight);
+scene.add(light);
 
-animate()
+animate();
