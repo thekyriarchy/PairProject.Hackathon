6619675e667d1bd69e56589c06de255ff4271348
Molly Hanes <mollyfhanes@gmail.com>
2018-03-19 16:03:23 -0500

    he has eyes!

--- script.js
+++ script.js
@@ -1,62 +1,85 @@
-var scene = new THREE.Scene();
+var scene = new THREE.Scene()
 var camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+	75,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	1000
+)
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+var renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
 
-var texture = new THREE.TextureLoader().load("snow.jpg");
-texture.wrapS = THREE.RepeatWrapping;
-texture.wrapT = THREE.RepeatWrapping;
-texture.repeat.set(4, 4);
+var texture = new THREE.TextureLoader().load('snow.jpg')
+texture.wrapS = THREE.RepeatWrapping
+texture.wrapT = THREE.RepeatWrapping
+texture.repeat.set(4, 4)
 
 // body
-var geometryBase = new THREE.SphereGeometry(6, 32, 32);
-var geometryAbdomen = new THREE.SphereGeometry(5, 32, 32);
-var geometryHead = new THREE.SphereGeometry(4, 32, 32);
+var geometryBase = new THREE.SphereGeometry(6, 32, 32)
+var geometryAbdomen = new THREE.SphereGeometry(5, 32, 32)
+var geometryHead = new THREE.SphereGeometry(4, 32, 32)
 var materialBody = new THREE.MeshBasicMaterial({
-  color: 0xffffff,
-  map: texture
-});
-var base = new THREE.Mesh(geometryBase, materialBody);
-var abdomen = new THREE.Mesh(geometryAbdomen, materialBody);
-var head = new THREE.Mesh(geometryHead, materialBody);
+	color: 0xffffff,
+	map: texture
+})
+var base = new THREE.Mesh(geometryBase, materialBody)
+var abdomen = new THREE.Mesh(geometryAbdomen, materialBody)
+var head = new THREE.Mesh(geometryHead, materialBody)
 
-// face
-var geometryNose = new THREE.ConeGeometry(1, 3, 8);
-var materialNose = new THREE.MeshBasicMaterial({ color: 0xffa500 });
-var nose = new THREE.Mesh(geometryNose, materialNose).rotateX(1.5);
-var face = new THREE.Group();
-abdomen.position.y = 6.5;
-head.position.y = 12;
-nose.position.z = 5;
-nose.position.y = 12;
-scene.add(base);
-scene.add(abdomen);
-face.add(head);
-face.add(nose);
-scene.add(face);
+//nose
+var geometryNose = new THREE.ConeGeometry(1, 3, 8)
+var materialNose = new THREE.MeshBasicMaterial({ color: 0xffa500 })
+var nose = new THREE.Mesh(geometryNose, materialNose).rotateX(1.5)
 
-camera.position.z = 32;
-camera.position.y = 12;
+//eyes
+var geometryEye = new THREE.SphereGeometry(0.3, 32, 32)
+var materialEye = new THREE.MeshBasicMaterial({ color: 0x000000 })
+var leftEye = new THREE.Mesh(geometryEye, materialEye)
+var rightEye = new THREE.Mesh(geometryEye, materialEye)
+
+//face position
+var face = new THREE.Group()
+
+nose.position.z = 5
+nose.position.y = 12
+leftEye.position.z = 3.9
+leftEye.position.x = -1
+leftEye.position.y = 13
+rightEye.position.z = 3.9
+rightEye.position.x = 1
+rightEye.position.y = 13
+
+face.add(head)
+face.add(nose)
+face.add(leftEye)
+face.add(rightEye)
+
+scene.add(face)
+
+//body position
+
+abdomen.position.y = 6.5
+head.position.y = 12
+
+scene.add(base)
+scene.add(abdomen)
+
+//camera position
+
+camera.position.z = 32
+camera.position.y = 12
+
+//animation
 
 function animate() {
-  requestAnimationFrame(animate);
-  face.rotation.y += 0.1;
-  abdomen.rotation.y += 0.1;
-  base.rotation.y += 0.1;
-  renderer.render(scene, camera);
+	requestAnimationFrame(animate)
+
+	face.rotation.y += 0.1
+	abdomen.rotation.y += 0.1
+	base.rotation.y += 0.1
+
+	renderer.render(scene, camera)
 }
 
-ambientLight = new THREE.AmbientLight(0x333333); // 0.2
-light = new THREE.DirectionalLight(0xffffff, 1.0);
-
-scene.add(ambientLight);
-scene.add(light);
-
-animate();
+animate()
