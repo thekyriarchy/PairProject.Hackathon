d4fcb56dbbb954feedcbcf0d89bcc1c3552bbfd7
Kẏra <kxra@riseup.net>
2018-03-19 16:45:29 -0400

    added nose and sput that bad boy around

--- script.js
+++ script.js
@@ -1,47 +1,62 @@
-var scene = new THREE.Scene()
+var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(
-	75,
-	window.innerWidth / window.innerHeight,
-	0.1,
-	1000
-)
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
-var renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-var texture = new THREE.TextureLoader().load('snow.jpg')
-texture.wrapS = THREE.RepeatWrapping
-texture.wrapT = THREE.RepeatWrapping
-texture.repeat.set(4, 4)
+var texture = new THREE.TextureLoader().load("snow.jpg");
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+texture.repeat.set(4, 4);
 
-var geometryBase = new THREE.SphereGeometry(6, 32, 32)
-var geometryAbdomen = new THREE.SphereGeometry(5, 32, 32)
-var geometryHead = new THREE.SphereGeometry(4, 32, 32)
-var material = new THREE.MeshBasicMaterial({ color: 0xffffff, map: texture })
-var base = new THREE.Mesh(geometryBase, material)
-var abdomen = new THREE.Mesh(geometryAbdomen, material)
-var head = new THREE.Mesh(geometryHead, material)
-abdomen.position.y = 6.5
-head.position.y = 12
-scene.add(base)
-scene.add(abdomen)
-scene.add(head)
+// body
+var geometryBase = new THREE.SphereGeometry(6, 32, 32);
+var geometryAbdomen = new THREE.SphereGeometry(5, 32, 32);
+var geometryHead = new THREE.SphereGeometry(4, 32, 32);
+var materialBody = new THREE.MeshBasicMaterial({
+  color: 0xffffff,
+  map: texture
+});
+var base = new THREE.Mesh(geometryBase, materialBody);
+var abdomen = new THREE.Mesh(geometryAbdomen, materialBody);
+var head = new THREE.Mesh(geometryHead, materialBody);
 
-camera.position.z = 16
-camera.position.y = 4
+// face
+var geometryNose = new THREE.ConeGeometry(1, 3, 8);
+var materialNose = new THREE.MeshBasicMaterial({ color: 0xffa500 });
+var nose = new THREE.Mesh(geometryNose, materialNose).rotateX(1.5);
+var face = new THREE.Group();
+abdomen.position.y = 6.5;
+head.position.y = 12;
+nose.position.z = 5;
+nose.position.y = 12;
+scene.add(base);
+scene.add(abdomen);
+face.add(head);
+face.add(nose);
+scene.add(face);
+
+camera.position.z = 32;
+camera.position.y = 12;
 
 function animate() {
-	requestAnimationFrame(animate)
-	//sphere.rotation.x += 0.2
-	//sphere.rotation.y += 0.2
-	renderer.render(scene, camera)
+  requestAnimationFrame(animate);
+  face.rotation.y += 0.1;
+  abdomen.rotation.y += 0.1;
+  base.rotation.y += 0.1;
+  renderer.render(scene, camera);
 }
 
-ambientLight = new THREE.AmbientLight(0x333333) // 0.2
-light = new THREE.DirectionalLight(0xffffff, 1.0)
+ambientLight = new THREE.AmbientLight(0x333333); // 0.2
+light = new THREE.DirectionalLight(0xffffff, 1.0);
 
-scene.add(ambientLight)
-scene.add(light)
+scene.add(ambientLight);
+scene.add(light);
 
-animate()
+animate();
